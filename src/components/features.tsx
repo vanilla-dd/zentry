c39537b37fc294b0daccfa33c00b3e765354fb74
vanilla-dd <akashsingh123456765432@gmail.com>
2024-12-23 18:01:32 +0530

    fix: animation cleanup

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -10,43 +10,19 @@ const BentoCard = ({
   src,
   title,
   description,
+  className,
+  children,
 }: {
-  src: string;
+  src?: string;
   title: React.ReactNode;
-  description: string;
+  description?: string;
+  className?: string;
+  children?: React.ReactNode;
 }) => {
-  return (
-    <div className="relative size-full overflow-hidden rounded-lg">
-      {src && (
-        <video
-          src={src}
-          loop
-          autoPlay
-          muted
-          playsInline
-          className="absolute left-0 top-0 size-full object-cover object-center"
-        />
-      )}
-      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
-        <div>
-          <h1 className="bento-title special-font">{title}</h1>
-          {description && (
-            <p className="mt-3 max-w-64 text-sm opacity-90 transition-opacity duration-300 hover:opacity-100 md:text-base">
-              {description}
-            </p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Features = () => {
-  const cardsRef = useRef<HTMLDivElement[] | null[]>([]);
-
+  const cardRef = useRef<HTMLDivElement | null>(null);
   useGSAP(() => {
-    cardsRef.current.forEach((card) => {
-      gsap.set(card, {
+    if (cardRef.current) {
+      gsap.set(cardRef.current, {
         opacity: 0,
         y: 100,
         rotateX: -40,
@@ -54,23 +30,51 @@ const Features = () => {
         transformOrigin: 'center top',
       });
 
-      // Create animation
-      gsap.to(card, {
+      gsap.to(cardRef.current, {
         opacity: 1,
         y: 0,
         rotateX: 0,
         duration: 0.5,
         scrollTrigger: {
-          trigger: card,
-          start: 'top bottom-=100',
+          trigger: cardRef.current,
+          start: 'top bottom-=20',
           end: 'top center',
           toggleActions: 'play none none reverse',
           markers: false,
         },
       });
-    });
+    }
   }, []);
+  return (
+    <div className={className} ref={cardRef}>
+      <div className="relative size-full overflow-hidden rounded-lg">
+        {src && (
+          <video
+            src={src}
+            loop
+            autoPlay
+            muted
+            playsInline
+            className="absolute left-0 top-0 size-full object-cover object-center"
+          />
+        )}
+        <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+          <div>
+            <h1 className="bento-title special-font">{title}</h1>
+            {description && (
+              <p className="mt-3 max-w-64 text-sm opacity-90 transition-opacity duration-300 hover:opacity-100 md:text-base">
+                {description}
+              </p>
+            )}
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+const Features = () => {
   return (
     <section className="bg-black pb-52">
       <div className="container mx-auto px-3 md:px-10">
@@ -85,10 +89,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div
-          ref={(el) => (cardsRef.current[0] = el)}
-          className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-lg md:h-[65vh]"
-        >
+        <div className="grid auto-rows-fr grid-cols-2 gap-4">
           <BentoCard
             src="/videos/feature-1.mp4"
             title={
@@ -96,81 +97,59 @@ const Features = () => {
                 radia<b>n</b>t
               </>
             }
+            className="col-span-2 row-span-1 overflow-hidden rounded-lg border border-white/40"
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
           />
-        </div>
 
-        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-          <div
-            ref={(el) => (cardsRef.current[1] = el)}
-            className="row-span-1 md:col-span-1 md:row-span-2"
-          >
-            <BentoCard
-              src="/videos/feature-2.mp4"
-              title={
-                <>
-                  zig<b>m</b>a
-                </>
-              }
-              description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
-            />
-          </div>
+          <BentoCard
+            src="/videos/feature-2.mp4"
+            title={
+              <>
+                zig<b>m</b>a
+              </>
+            }
+            className="col-span-2 row-span-2 overflow-hidden rounded-lg border border-white/40 md:col-span-1"
+            description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+          />
 
-          <div
-            ref={(el) => (cardsRef.current[2] = el)}
-            className="row-span-1 ms-32 md:col-span-1 md:ms-0"
-          >
-            <BentoCard
-              src="/videos/feature-3.mp4"
-              title={
-                <>
-                  n<b>e</b>xus
-                </>
-              }
-              description="A gamified social hub, adding a new dimension of play to your identity, Web3 engagement and social interaction."
-            />
-          </div>
+          <BentoCard
+            src="/videos/feature-3.mp4"
+            title={
+              <>
+                n<b>e</b>xus
+              </>
+            }
+            className="col-span-2 row-span-1 row-start-2 ms-14 rounded-lg border border-white/40 md:col-span-1 md:row-start-auto md:ms-0"
+            description="A gamified social hub, adding a new dimension of play to your identity, Web3 engagement and social interaction."
+          />
 
-          <div
-            ref={(el) => (cardsRef.current[3] = el)}
-            className="me-14 md:col-span-1 md:me-0"
-          >
-            <BentoCard
-              src="/videos/feature-4.mp4"
-              title={
-                <>
-                  az<b>u</b>l
-                </>
-              }
-              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
-            />
-          </div>
+          <BentoCard
+            src="/videos/feature-4.mp4"
+            title={
+              <>
+                az<b>u</b>l
+              </>
+            }
+            className="col-span-2 row-span-1 row-start-3 me-20 overflow-hidden rounded-lg border border-white/40 md:col-span-1 md:row-start-auto md:me-0"
+            description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+          />
 
-          <div
-            ref={(el) => (cardsRef.current[4] = el)}
-            className="rounded-lg bg-violet-300"
-          >
-            <div className="flex size-full flex-col justify-between p-5">
-              <h1 className="bento-title special-font max-h-64 text-black">
+          <BentoCard
+            title={
+              <h1 className="bento-title special-font text-black">
                 Mor<b>e</b> co<b>m</b>ing so<b>o</b>n
               </h1>
-              <TiLocationArrow className="m-5 scale-[5] self-end" />
-            </div>
-          </div>
-
-          <div
-            ref={(el) => (cardsRef.current[5] = el)}
-            className="overflow-hidden rounded-lg"
+            }
+            className="col-span-1 row-span-1 flex flex-col justify-between rounded-lg bg-violet-300 p-5"
           >
-            <video
-              src="/videos/feature-5.mp4"
-              loop
-              muted
-              autoPlay
-              playsInline
-              className="size-full object-cover object-center"
-            />
-          </div>
+            <TiLocationArrow className="m-5 scale-[5] self-end" />
+          </BentoCard>
+
+          <BentoCard
+            src="/videos/feature-5.mp4"
+            title=""
+            className="col-span-1 row-span-1 hidden overflow-hidden rounded-lg md:block"
+          />
         </div>
       </div>
     </section>
